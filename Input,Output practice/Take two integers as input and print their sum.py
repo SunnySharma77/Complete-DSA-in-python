b92a55d5453eeebe7c1e968206157bef15a25cd9
Sunny Sharma <work77sharmasunny@gmail.com>
2025-08-09 18:08:10 +0530

    Create Take two integers as input and print their sum.py

---
+++ Input,Output practice/Take two integers as input and print their sum.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+total = a + b
+print("Sum:", total)
+
+#Example run:
+Enter first integer: 5
+Enter second integer: 7
+Sum: 12
+
+# Competitive programming style
+a, b = map(int, input().split())
+print(a + b)
