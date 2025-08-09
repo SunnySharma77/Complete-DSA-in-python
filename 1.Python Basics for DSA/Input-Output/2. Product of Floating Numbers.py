b905fa297f1ac34d5c3e99a7dff11c7111e30bf2
Sunny Sharma <work77sharmasunny@gmail.com>
2025-08-09 18:18:40 +0530

    Create 2. Product of Floating Numbers.py

---
+++ 1.Python Basics for DSA/Input-Output/2. Product of Floating Numbers.py
@@ -0,0 +1,9 @@
+x = float(input("Enter first decimal number: "))
+y = float(input("Enter second decimal number: "))
+
+product = x * y
+
+print("The product is:", round(product, 2))
+
+#Explanation:
+float() converts input to decimal. round(product, 2) keeps 2 decimal places
